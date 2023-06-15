0eddfea768d0bb2701dfa11811c358d4213b5c9e
janpf <janpf@users.noreply.github.com>
2023-06-15 19:09:37 +0200

    working!

--- SVLM.py
+++ SVLM.py
@@ -1,83 +1,63 @@
 import numpy as np
 from sklearn import svm
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
 
-# suppose you have your text corpus as a list of sentences
-corpus = ["ababababab"] * 100  # big data
+# Suppose you have your text corpus as a list of sentences
+corpus = ["ABABABABA", "CDCDCDCD", "EFEFEFEF"]
 
-# tokenize the corpus characterwise by their ASCII code points
+# Tokenize the corpus characterwise by their ASCII code points
 tokens = [[ord(char) for char in sentence] for sentence in corpus]
 
-# create input subsequences and corresponding next character for every single subsequence of tokens
+# Create input subsequences and corresponding next character for every single subsequence of tokens
 contexts, targets = [], []
-
 for sequence in tokens:
     for i in range(1, len(sequence)):
         # Consider only the last 20 characters
         contexts.append(sequence[max(i - 20, 0) : i])
         targets.append(sequence[i])
 
-## flatten the list of contexts to use with CountVectorizer
-contexts = [" ".join(map(str, context)) for context in contexts]
+# Transform contexts to a binary matrix representation
+X = np.zeros((len(contexts), 256))
+for i, context in enumerate(contexts):
+    for ascii_val in context:
+        X[i, ascii_val] += 1
 
+# Apply the weights
+weights = [1 / (i + 1) for i in range(1, X.shape[1] + 1)]
+X = X * weights
 
-# custom transformer that applies the weights
-class CustomTransformer:
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        weights = [1 / (i + 1) for i in range(X.shape[1], 0, -1)]
-        matrix = np.tril(np.ones((X.shape[1], X.shape[1])) * weights)
-        return X.dot(matrix)
-
-
-# create a pipeline that uses CountVectorizer and applies the weights
-pipeline = make_pipeline(
-    CountVectorizer(tokenizer=lambda x: x.split(" "), dtype=np.float64, ngram_range=(1, 2)),
-    # CustomTransformer(),
-)
-# transform the contexts
-X = pipeline.fit_transform(contexts)
-# encode the target characters as integers
+# Encode the target characters as integers
 y = [int(char) for char in targets]
 
-# split into training and test sets
+# Split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# create and train the multiclass SVM
-model = svm.SVC(
-    decision_function_shape="ovr",
-    kernel="linear",
-)
+# Create and train the multiclass SVM
+model = svm.SVC(decision_function_shape="ovo")
 model.fit(X_train, y_train)
 
-# make predictions on the test set
-# predictions = model.predict(X_test)
+# Make predictions on the test set
+predictions = model.predict(X_test)
 
 
-def decode(model, pipeline, initial_context, length):
+def decode(model, initial_context, length):
     text = initial_context
     for _ in range(length):
-        # Vectorize the current context
-        X = pipeline.transform([text])
+        # Transform the current context into the binary matrix representation
+        X = np.zeros((1, 256))
+        context = [ord(char) for char in text[-20:]]
+        for ascii_val in context:
+            X[0, ascii_val] += 1
+        X = X * weights
         # Predict the next character
         prediction = model.predict(X)
         # Decode the prediction
         next_char = chr(int(prediction))
-
         # Add the predicted character to the text
         text += next_char
     return text
 
 
 # Use the decoder to generate text
-generated_text = decode(
-    model,
-    pipeline,
-    "a",
-    50,
-)
+generated_text = decode(model, "AB", 30)
 print(generated_text)
